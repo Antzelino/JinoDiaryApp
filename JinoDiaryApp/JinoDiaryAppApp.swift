b3e73ef204998895dd5647de03ea58c8bed04996
Angelino Storm <anxhelino.mehmeti@sinch.com>
2025-03-08 20:47:24 +0100

    Initial commit

---
+++ JinoDiaryApp/JinoDiaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JinoDiaryAppApp.swift
+//  JinoDiaryApp
+//
+//  Created by Anxhelino Mehmeti on 2025-03-08.
+//
+
+import SwiftUI
+
+@main
+struct JinoDiaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
